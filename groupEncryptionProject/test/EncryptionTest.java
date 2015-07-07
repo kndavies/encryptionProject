636f7e4b030728c21fb3b1c1f7e8a8b4fe8b7c53
kndavies <kdavies@wccnet.edu>
2015-07-07 15:02:43 -0400

    Added a test

---
+++ groupEncryptionProject/test/EncryptionTest.java
@@ -0,0 +1,14 @@
+package groupEncryptionProject;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class EncryptionTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
